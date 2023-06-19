6d14ae89932b3a09db5f507aa4d3bc252d6afaa1
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-19 18:32:39 +0200

    adding comments

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      // Variable to store the autoplay interval
       autoplay: null,
+      // Current index of the displayed image
       currentIndex: 0,
       images: [
         {
@@ -38,39 +40,49 @@ createApp({
   },
   computed: {
     isLastPicture() {
+      // Check if the current index is the last image
       return this.currentIndex === this.images.length - 1;
     },
     isFirstPicture() {
+      // Check if the current index is the first image
       return this.currentIndex === 0;
     },
   },
   methods: {
     goToNext() {
       if (this.isLastPicture) {
+        // If it's the last image, go back to the first image
         this.currentIndex = 0;
       } else {
+        // Otherwise, move to the next image
         this.currentIndex++;
       }
     },
     goToPrev() {
       if (this.isFirstPicture) {
+        // If it's the first image, go to the last image
         this.currentIndex = 4;
       } else {
+        // Otherwise, move to the previous image
         this.currentIndex--;
       }
     },
     setCurrentIndex(targetIndex) {
+      // Set the current index to the specified target index
       this.currentIndex = targetIndex;
     },
     stopAutoPlay() {
+      // Clear the autoplay interval
       clearInterval(this.autoplay);
     },
     startAutoPlay() {
+      // Start autoplay by setting an interval to go to the next image
       this.autoplay = setInterval(this.goToNext, 3000);
     },
   },
   // LifeCycle Methods
   mounted() {
+    // Start autoplay when the component is mounted
     this.startAutoPlay();
   },
 }).mount("#root");
